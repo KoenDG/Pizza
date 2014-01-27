fa2110b171d5cb4df269ad373c7e120c906b7923
Koen De Groote <kdg.dev@gmail.com>
2014-01-27 23:16:55 +0100

    Added code for images and pizzas

--- bestel.php
+++ bestel.php
@@ -1,3 +1,30 @@
 <?php
 
 session_start();
+use Doctrine\Common\ClassLoader;
+
+require_once('Doctrine/Common/ClassLoader.php');
+$classLoader = new ClassLoader("Pizza", "src");
+$classLoader->register();
+
+require_once('Libraries/Twig/Autoloader.php');
+Twig_Autoloader::register();
+
+$pizzas = array(array(
+    "name" => "cheese",
+    "description" => "4 kazen pizza"
+),array(
+    "name" => "hawai",
+    "description" => "Pizza hawai"
+),array(
+    "name" => "margherita",
+    "description" => "Pizza Margherita"
+),array(
+    "name" => "pepperoni",
+    "description" => "Pizza Pepperoni"
+));
+
+$loader = new Twig_Loader_Filesystem("src/Pizza/Presentation");
+$twig = new Twig_Environment($loader);
+$view = $twig->render("bestel.twig",array("pizzas" => $pizzas));
+print($view);
